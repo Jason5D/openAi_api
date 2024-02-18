33d227433a4a1a2f8b7fe8277226c4bae47c3bab
Jason <jason5donoghue@gmail.com>
2024-02-18 11:46:31 +0000

    getting a response and printing to the console

--- chatCompletions.js
+++ chatCompletions.js
@@ -1,15 +1,26 @@
-import 'dotenv/config'
+import "dotenv/config";
 import OpenAI from "openai";
 
 const openai = new OpenAI();
 
 async function main() {
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    messages: [
+      { role: "system", content: "You are a helpful assistant." },
+      { role: "user", content: "What do software engineers do?" },
+      {
+        role: "assistant",
+        content: "Software engineers write code to build software.",
+      },
+    ],
     model: "gpt-3.5-turbo",
+    temperature: 0.2,
+    // response_format: { type: "json_object" },
   });
 
-  console.log(completion.choices[0]);
+  console.log(completion.choices[0].message.role);
+  console.log(completion.choices[0].message.content);
+  // console.log(completion["usage"]["total_tokens"]);
 }
 
 main();
